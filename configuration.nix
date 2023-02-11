a4087ad76f0bc4530908f64eb1c00940cd9761e1
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2023-02-11 07:09:32 +0700

    remove gg

--- configuration.nix
+++ configuration.nix
@@ -236,7 +236,6 @@ in {
       # Versioning
       git
       gitui
-      gg-scm
       git-cola
 
       # IDE unwrapped
@@ -693,7 +692,7 @@ in {
       useBabelfish = true;
       shellAbbrs = {
         h = "hx";
-        g = "gg";
+        g = "git";
 
         ll = "ls -lha";
         lsf = "lsblk -o name,fstype,fsavail,fsused,size,mountpoint";
@@ -705,6 +704,66 @@ in {
         nrs = "doas nixos-rebuild switch";
         nsgc = "doas nix-store --gc";
 
+        ga = "git add";
+        gaa = "git add --all";
+
+        gb = "git branch";
+        gbl = "git blame -b -w";
+        gbr = "git branch --remote";
+
+        gcm = "git commit -m";
+        gcam = "git commit --amend";
+
+        gcb = "git checkout -b";
+        gck = "git checkout main";
+
+        gcf = "git config --list";
+        gcl = "git clone --recursive";
+        gcln = "git clean -fd";
+        gcp = "git cherry-pick";
+
+        gd = "git diff";
+        gdca = "git diff --cached";
+
+        gfs = "git fetch sh";
+        gfg = "git fetch gh";
+        gfl = "git fetch gl";
+        gfa = "git fetch --all --prune";
+
+        gignore = "git update-index --assume-unchanged";
+
+        gls = "git log --stat";
+        glsp = "git log --stat -p";
+        glg = "git log --graph";
+        glo = "git log --oneline --decorate";
+
+        gm = "git merge";
+        gmsm = "git merge sh/main";
+        gmgm = "git merge gh/main";
+        gmlm = "git merge gl/main";
+        gmt = "git mergetool --no-prompt";
+
+        gpsm = "git push -u sh main";
+        gpgm = "git push -u gh main";
+        gplm = "git push -u gl main";
+
+        gr = "git remote";
+        gra = "git remote add";
+        grh = "git reset HEAD";
+        grhh = "git reset HEAD --hard";
+
+        gs = "git status";
+        gsps = "git show --pretty=short --show-signature";
+        gsts = "git stash show --text";
+        gsu = "git submodule update";
+
+        gts = "git tag -s";
+        gta = "git tag -a";
+
+        gur = "git pull --rebase";
+        gusm = "git pull sh main";
+        gugm = "git pull gh main";
+        gulm = "git pull gl main";
       };
     };
   };
