a562b8a8062e79f1d9db88a873107ca26ff61b1b
Hervy Qurrotul Ainur Rozi <hervyqa@proton.me>
2023-02-27 12:29:10 +0700

    printer pkgs move to services

--- services/printing/default.nix
+++ services/printing/default.nix
@@ -9,6 +9,18 @@
       enable = true;
       stateless = true;
       webInterface = false;
+      drivers = with pkgs; [
+        epson-escpr
+        epson-escpr2
+        foomatic-db
+        foomatic-filters
+        gutenprint
+        hplip
+        splix
+      ]
+    };
+    system-config-printer = {
+      enable = true;
     };
   };
 }
